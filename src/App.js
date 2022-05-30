213ad238a03fd1ba9a615f65d1a4a4fe6af2ca41
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-05-30 18:26:41 +0600

    🛠️code cleanup

--- src/App.js
+++ src/App.js
@@ -13,12 +13,7 @@ import Airdrop from "./pages/Airdrop";
 import StakeDrop from "./pages/StakeDrop";
 import OsmosisClaimPage from "./pages/claim/OsmosisClaimPage";
 
-import CosmosCalculationPage from "./pages/StakeDrop/CosmosCalculationPage";
-import PersistenceCalculationPage from "./pages/StakeDrop/persistence/CalculationPage";
-import TerraCalculationPage from "./pages/StakeDrop/terra/CalculationPage";
-import ComdexCalculationPage from "./pages/StakeDrop/comdex/CalculationPage";
-import JunoCalculationPage from "./pages/StakeDrop/juno/CalculationPage";
-import StargazeCalculationPage from "./pages/StakeDrop/stargaze/CalculationPage";
+import CalculationPage from "./pages/StakeDrop/CalculationPage";
 
 const App = () => {
   const location = useLocation();
@@ -50,28 +45,8 @@ const App = () => {
       path: "/stakedrop",
     },
     {
-      component: CosmosCalculationPage,
-      path: "/stakedrop/cosmos",
-    },
-    {
-      component: PersistenceCalculationPage,
-      path: "/stakedrop/persistence",
-    },
-    {
-      component: TerraCalculationPage,
-      path: "/stakedrop/terra",
-    },
-    {
-      component: ComdexCalculationPage,
-      path: "/stakedrop/comdex",
-    },
-    {
-      component: JunoCalculationPage,
-      path: "/stakedrop/juno",
-    },
-    {
-      component: StargazeCalculationPage,
-      path: "/stakedrop/stargaze",
+      component: CalculationPage,
+      path: "/stakedrop/:id",
     },
     {
       component: OsmosisClaimPage,
