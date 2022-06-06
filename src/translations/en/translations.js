b50e38ef275b7c49ce6bbe833d0d17aec7e39830
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-06-06 14:34:43 +0600

    Success modal and account exist modal added (#15)
    * opensea claim
    
    * 💅🛠️🦄📕success screen added
    
    * 🛠️bug fix
    
    * 📕🛠️💅argument, data, logic updated
    
    Co-authored-by: Ankit Kumar <ankit.kr554@gmail.com>

--- src/translations/en/translations.js
+++ src/translations/en/translations.js
@@ -108,7 +108,10 @@ const English = {
     AIRDROP_MODAL_OPENSEA_STEP1: airDropData.openSeaSignModal.step1,
     AIRDROP_MODAL_OPENSEA_STEP2: airDropData.openSeaSignModal.step2,
     AIRDROP_MODAL_OPENSEA_PLACEHOLDER: airDropData.openSeaSignModal.placeholder,
-    AIRDROP_MODAL_OPENSEA_DONT_Have: airDropData.openSeaSignModal.dontHave,
+    AIRDROP_MODAL_OPENSEA_DONT_HAVE: airDropData.openSeaSignModal.dontHave,
+    AIRDROP_MODAL_OPENSEA_SUCCESS_TITLE:
+      airDropData.openSeaSignModal.successTitle,
+    AIRDROP_MODAL_OPENSEA_EXIST_TITLE: airDropData.openSeaSignModal.existTitle,
 
     // Osmosis Claim Page
     // title section
